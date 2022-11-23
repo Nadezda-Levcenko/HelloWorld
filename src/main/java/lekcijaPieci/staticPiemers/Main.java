d370f73625fc24b2ae4f564637dfad21d456e4cb
Nadezda-Levcenko <esperanse15@inbox.lv>
2022-11-23 10:00:01 +0200

    pievienota lekcija Pieci

---
+++ src/main/java/lekcijaPieci/staticPiemers/Main.java
@@ -0,0 +1,10 @@
+package lekcijaPieci.staticPiemers;
+
+public class Main {
+    public static void main(String[] args) {
+        Cilveks persona1 = new Cilveks("Pēteris");
+        persona1.printName();
+        Cilveks persona2 = new Cilveks("Jana");
+        persona2.printName();
+    }
+}
