0371c83ddd2ac08425f494b0fffbb674067ab61d
Nadezda-Levcenko <esperanse15@inbox.lv>
2022-11-25 04:05:26 +0200

    pievienoju Astoto lekciju

---
+++ src/main/java/lekcijaAstoni/pageObjects/CartPage.java
@@ -0,0 +1,19 @@
+package lekcijaAstoni.pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CartPage extends BasePage{
+
+    private By checkoutButton = By.id("checkout");
+
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public WebElement getCheckoutButton() {
+        return driver.findElement(checkoutButton);
+    }
+
+}
---
+++ src/main/java/lekcijaAstoni/pageObjects/ProductsPage.java
@@ -0,0 +1,18 @@
+package lekcijaAstoni.pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ProductsPage extends BasePage {
+    private By cartButton = By.id("shopping_cart_container");
+
+    public ProductsPage(WebDriver parluks) {
+        super(parluks);
+    }
+
+    public WebElement getCartButton() {
+        return driver.findElement(cartButton);
+    }
+
+}
